3da09fea9baec8f884594c4fcfb299cbbddecbd4
Alvaro Garzón <alvaro.develops@gmail.com>
2022-11-21 22:17:16 +0100

    merge development

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import MenuDashboard from '../MenuDashboard';
+import HeaderDashboard from '../HeaderDashboard';
 import {Container} from './MainContainerStyled';
 
 const MainContainer = ({children}) => {
 const { pathname } = useLocation();
 
 	return (
-	   <Container className="MAIN" path={pathname}>{children}</Container>
-	)
+      <div>
+         <MenuDashboard />
+         <HeaderDashboard />
+         <Container className="MAIN" path={pathname}>
+            {children}
+         </Container>
+      </div>
+   );
 }
  
 export default MainContainer;
