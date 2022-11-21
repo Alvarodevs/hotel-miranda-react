6120529fd75f682fde570eff5c0c52815ff858d5
Alvaro Garzón <alvaro.develops@gmail.com>
2022-11-21 22:17:41 +0100

    main container structure

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import MenuDashboard from '../MenuDashboard';
-import HeaderDashboard from '../HeaderDashboard';
 import {Container} from './MainContainerStyled';
 
 const MainContainer = ({children}) => {
 const { pathname } = useLocation();
 
 	return (
-      <div>
-         <MenuDashboard />
-         <HeaderDashboard />
-         <Container className="MAIN" path={pathname}>
-            {children}
-         </Container>
-      </div>
-   );
+	   <Container className="MAIN" path={pathname}>{children}</Container>
+	)
 }
  
 export default MainContainer;
