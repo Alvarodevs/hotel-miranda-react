41f8f00a7c17747b81ecac99c0ebd88f01bc2056
Alvaro Garzón <alvaro.develops@gmail.com>
2022-12-18 23:18:15 +0100

    actions and login reducer to fix logout

--- src/store/loginReducer.js
+++ src/store/loginReducer.js
@@ -1,32 +1,36 @@
 import { LOGIN, LOGOUT, UPDATE_EMAIL, UPDATE_USERNAME } from './actionTypes';
 
 const loginReducer = (state, action) => {
-	const {type, payload} = action;
+   const { type, payload } = action;
 
-	switch (type) {
-		case LOGIN:
-			return { ...state, ...payload }
-		case LOGOUT:
-			return { isAuth: payload }
-		case UPDATE_EMAIL:
-			return { 
-				...state, 
-					user: {
-						...state.user,
-						email: payload
-					} 
-			}
-		case UPDATE_USERNAME:
-			return {
-				...state,
-				user: {
-					...state.user,
-					name: payload
-				}
-			}
-		default:
-			return state;
-	}
-}
+   switch (type) {
+      case LOGIN:
+         return { ...state, ...payload };
+
+      case LOGOUT:
+         return {
+            isAuth: false
+         };
+			
+      case UPDATE_EMAIL:
+         return {
+            ...state,
+            user: {
+               ...state.user,
+               email: payload,
+            },
+         };
+      case UPDATE_USERNAME:
+         return {
+            ...state,
+            user: {
+               ...state.user,
+               name: payload,
+            },
+         };
+      default:
+         return state;
+   }
+};
 
 export default loginReducer;
